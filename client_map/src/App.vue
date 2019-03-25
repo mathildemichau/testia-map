590e8145cf534bff56ba80dce01ad33aecd96977
Mathilde Michau <mathilde@Mathildes-MacBook-Pro.local>
2019-03-25 17:11:30 +0100

    Display a marker where the user clicked Calculates the distance between the position and the marker Get the country of where the user clicked

--- client_map/src/App.vue
+++ client_map/src/App.vue
@@ -1,23 +1,70 @@
 <template>
   <div id="app">
-    <map-leaflet/>
+    <show-location/>
+    <map-leaflet @newMarker="updateMarker" @newPosition="updatePosition"/>
   </div>
 </template>
 
 <script>
-import MapLeaflet from './components/MapLeaflet.vue'
+import MapLeaflet from "./components/MapLeaflet.vue";
+import ShowLocation from "./components/ShowLocation.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    MapLeaflet
+    MapLeaflet,
+    ShowLocation
+  },
+  data() {
+    return {
+      position: null,
+      distance: null,
+      markerPosition: null,
+      country: null
+    };
+  },
+  methods: {
+    updateMarker(coords) {
+      this.markerPosition = coords;
+      if(this.position) this.distance = this.getDistance(this.position, this.markerPosition);
+      this.getCountry();
+    },
+    updatePosition(coords) {
+      this.position = coords;
+    },
+    //Calculates distance between 2 points
+    getDistance(p1, p2) {
+      //Calculates radius of a value
+      const rad = x => (x * Math.PI) / 180;
+
+      const R = 6378137; // Earth’s mean radius in meter
+      const dLat = rad(p2.latitude - p1.latitude);
+      const dLong = rad(p2.longitude - p1.longitude);
+      let a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(rad(p1.latitude)) *
+          Math.cos(rad(p2.latitude)) *
+          Math.sin(dLong / 2) *
+          Math.sin(dLong / 2);
+      let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      let d = (R * c) / 1000;
+      return d.toFixed(2);
+    },
+    rad(x) {
+      return;
+    },
+    async getCountry() {
+      let res = await fetch(`http://api.geonames.org/countrySubdivisionJSON?lat=${this.markerPosition.latitude}&lng=${this.markerPosition.longitude}&username=mathildemichau`)
+      let json = await res.json()
+      this.country = json.countryName
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- client_map/src/components/MapLeaflet.vue
+++ client_map/src/components/MapLeaflet.vue
@@ -1,50 +1,56 @@
 <template>
-    <div id="leaflet-map">
-        map here
-    </div>
+  <div id="leaflet-map">map here</div>
 </template>
 
 <script>
-    import L from 'leaflet';
-    import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-    export default {
-        data(){
-            return {
-                map: null,
-                position: null
-            }
-        },
-        mounted() {
-            // Get the user's current location
-            if(!this.position) navigator.geolocation.getCurrentPosition(this.showPosition)
+export default {
+  data() {
+    return {
+      map: null,
+      position: null,
+    };
+  },
+  mounted() {
+    // Get the user's current location
+    if (!this.position)
+      navigator.geolocation.getCurrentPosition(this.showPosition);
 
-            //Creation off the map by default centered on Toulouse
-            this.map = L.map('leaflet-map', {
-            center: [43.6044622, 1.4442469],
-            zoom: 3
-            });
+    //Creation off the map by default centered on Toulouse
+    this.map = L.map("leaflet-map", {
+      center: [43.6044622, 1.4442469],
+      zoom: 3
+    });
 
-            L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(this.map);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
 
-        },
-        methods: {
-            showPosition(position){
-                this.position = position.coords
-            },
+    this.map.on("click", this.addMarker);
+  },
+  methods: {
+    showPosition(position) {
+      this.$emit("newPosition", position.coords);
+    },
+    addMarker(e) {
+      let popup = L.popup();
+      popup.setLatLng(e.latlng).openOn(this.map);
 
-        }
+      this.$emit("newMarker", {latitude: e.latlng.lat, longitude: e.latlng.lng})
     }
-
-
-    
-
+  }
+};
 </script>
 <style>
-    #leaflet-map { 
-        height: 90vh; 
-    }
+#leaflet-map {
+  height: 90vh;
+}
+.leaflet-popup-content-wrapper {
+  background: no-repeat url("../assets/logoTestia.jpg") ;
+  height: 35px;
+  width: 145px;
+}
 </style>
